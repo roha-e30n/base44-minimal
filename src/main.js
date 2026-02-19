7cd204a5feb4ed2768680128f60c5092b29dad16
roha-e30n <roha.e30n@gmail.com>
2026-02-19 01:21:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,5 +8,22 @@ const root = document.querySelector('#root');
 root.innerHTML = `
   <h1>Base44 Minimal!</h1>
   <p>Content without React.</p>
-  <div id="data">Load info...</div>
+  <div id="data">Info...</div>
 `;
+
+const loadTasks = async () => {
+  try {
+    document.querySelector('#data').innerHTML = `load`;
+    const taskList = await base44.entities.Task.list();
+    let taskHtml = 'Tasks:<br/>';
+    for (const task of taskList) {
+      taskHtml += `<li>${task.title || JSON.stringify(task)}</li>`;
+    }
+    document.querySelector('#data').innerHTML = taskHtml;
+  } catch (error) {
+    document.querySelector('#data').innerHTML = `error`;
+    console.error('Error loading tasks:', error);
+  }
+};
+
+loadTasks();
