5c89db735bc32b7eee8b0cf1b39e26368b88d7e8
roha-e30n <roha.e30n@gmail.com>
2026-02-23 00:10:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,19 +13,19 @@ root.innerHTML = `
 
 const loadData = async () => {
   try {
-    document.querySelector('#data').innerHTML = `load`;
+    document.querySelector('#data').innerHTML = `<div class="is-loading">load</div>`;
     const taskList = await base44.entities.Task.list();
     const noteList = await base44.entities.Note.list();
     let taskHtml = 'Data:<hr/>';
     for (const task of taskList) {
-      taskHtml += `<li>task: ${task.title || JSON.stringify(task)}</li>`;
+      taskHtml += `<li class="list-item item-task"><span class="item-label label-task">Task</span> ${task.title || JSON.stringify(task)}</li>`;
     }
     for (const note of noteList) {
-      taskHtml += `<li>note: ${note.message || JSON.stringify(note)}</li>`;
+      taskHtml += `<li class="list-item item-note"><span class="item-label label-note">Note</span> ${note.message || JSON.stringify(note)}</li>`;
     }
     document.querySelector('#data').innerHTML = taskHtml;
   } catch (error) {
-    document.querySelector('#data').innerHTML = `error`;
+    document.querySelector('#data').innerHTML = `<div class="is-error">error</div>`;
     console.error('Error loading data:', error);
   }
 };
