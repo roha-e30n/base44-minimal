b82d86e13fd3d18e564672e2832e469a684cc93d
roha-e30n <roha.e30n@gmail.com>
2026-02-23 18:03:10 +0100

    Create landing.js

---
+++ src/page-content/landing.js
@@ -0,0 +1,7 @@
+export const routeContent = [
+  // Deutsch
+  { path: 'de/index.html', lang: 'de', title: 'Startseite', type: 'landing' },
+  
+  // Englisch (Global)
+  { path: 'en/index.html', lang: 'en', title: 'Homepage', type: 'landing' }
+];
